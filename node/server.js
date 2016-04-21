be91882288004144b2cb234b67db5ff356c88263
EberDroid <sbl@MacBook-de-Eber.local>
2016-04-21 00:59:48 -0500

    Se agreg puerto 3000 y mensaje de comunidad

--- node/server.js
+++ node/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send("Hola DYS Edición 2 ");
 });
 
-app.listen(8080, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
